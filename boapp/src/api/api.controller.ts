33d5396629f17f430b39f5e5b5678982d28d9aad
TOConde <tomy_t.c@hotmail.com>
2024-11-10 17:33:02 -0300

    implemento buscado de cotizaciones por fecha desde hasta

--- boapp/src/api/api.controller.ts
+++ boapp/src/api/api.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { ApiService } from './api.service';
 
 @Controller('api')
 export class ApiController {
-  constructor(private readonly apiService: ApiService) {}
+  constructor(private readonly apiService: ApiService) { }
 
-  @Get('empresaDetails/:codEmpresa')
+  @Get('empresa/:codEmpresa/details')
   async getEmpresaDetails(@Param('codEmpresa') codEmpresa: string) {
     return this.apiService.getEmpresaDetails(codEmpresa);
+  }
+
+  //yyyy/mm/ddT00:00
+  @Get('empresa/:codEmpresa/cotizacion')
+  async getEmpresaCotizacion(@Param('codEmpresa') codEmpresa: string, @Query('fechaDesde') fechaDesde: string, @Query('fechaHasta') fechaHasta: string) {
+    return this.apiService.getEmpresaCotizacion(codEmpresa, fechaDesde, fechaHasta);
   }
 }
--- boapp/src/api/api.service.ts
+++ boapp/src/api/api.service.ts
@@ -14,4 +14,14 @@ export class ApiService {
       throw new Error('No se pudo obtener los datos');
     }
   }
+
+  async getEmpresaCotizacion(codEmpresa: string, fechaDesde: string, fechaHasta: string): Promise<any> {
+    try {
+      const response = await axios.get(`${this.apiUrl}/empresas/${codEmpresa}/cotizaciones?fechaDesde=${fechaDesde}&fechaHasta=${fechaHasta}`);
+      return response.data;
+    } catch (error) {
+      console.error('Error obteniendo detalles de empresa:', error);
+      throw new Error('No se pudo obtener los datos');
+    }
+  }
 }
