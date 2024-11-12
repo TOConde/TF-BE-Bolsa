02a6637ad99863b87acb3b7f620be2117236bf92
TOConde <tomy_t.c@hotmail.com>
2024-11-12 16:47:48 -0300

    implemento guardado en mi DB datos de la API

--- boapp/src/empresa/empresa.controller.ts
+++ boapp/src/empresa/empresa.controller.ts
@@ -1,10 +1,13 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Param, Post, Query } from "@nestjs/common";
 import { EmpresaService } from "./empresa.service";
 
 
-@Controller('empresas')
+@Controller('empresa')
 export class EmpresaController {
   constructor(private readonly empresaService: EmpresaService) {}
 
-  //geters etc
+  @Post(':codEmpresa/cotizacion')
+  async actualizarCotizacion(@Param('codEmpresa') codEmpresa: string, @Query('fechaDesde') fechaDesde: string, @Query('fechaHasta') fechaHasta: string,) {
+    return await this.empresaService.actualizarCotizacion(codEmpresa, fechaDesde, fechaHasta)
+  }
 }
--- boapp/src/empresa/empresa.module.ts
+++ boapp/src/empresa/empresa.module.ts
@@ -2,12 +2,16 @@ import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { EmpresaController } from "./empresa.controller";
 import { EmpresaService } from "./empresa.service";
+import { Cotizacion } from "./entities/cotizacion.entity";
+import { Empresa } from "./entities/empresa.entity";
+import { ApiService } from "src/api/api.service";
+import { EmpresaSeeder } from "./seed/empresa.seeder";
 
 
 @Module({
-  imports: [TypeOrmModule.forFeature([])],
+  imports: [TypeOrmModule.forFeature([Cotizacion, Empresa])],
   controllers: [EmpresaController],
-  providers: [EmpresaService],
-  exports: [EmpresaService],
+  providers: [EmpresaService, ApiService, EmpresaSeeder],
+  exports: [EmpresaService, EmpresaSeeder],
 })
 export class EmpresaModule {}
